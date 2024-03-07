bfd38f56ca3bc73795bfd4e3b6aa7afbbe79b340
michal <oolejnikmichal@gmail.com>
2024-03-07 14:03:09 +0100

    clean code day6, two solutions, naive and math

--- day6/math_sol.py
+++ day6/math_sol.py
@@ -18,6 +18,9 @@ pierw = b**2 - 4*a*c
 root1 = (-b + math.sqrt(pierw)) / (2*a)
 root2 = (-b - math.sqrt(pierw)) / (2*a)
 
+# sort the roots
+root1, root2 = sorted([root1, root2])
+
 # need only numbers > 0
 root1 = max(0, root1)
 root2 = max(0, root2)
@@ -25,9 +28,14 @@ root2 = max(0, root2)
 root1 = math.floor(root1)
 root2 = math.floor(root2)
 
-counter = 0
-print(root1, root2)
-for i in range(root1,root2):
-    counter += 1
+
+# bad habits counter implementation
+# counter = 0
+# for i in range(root1,root2):
+#     counter += 1
+
+
+# proper counter implementation
+counter = len([i for i in range(root1, root2)])
 
 print(counter)
