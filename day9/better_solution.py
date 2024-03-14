13cb6b3acbb5e6c27c8db1fe8e4fd3716087a1fb
michal <oolejnikmichal@gmail.com>
2024-03-14 12:04:58 +0100

    solutions for day9

--- day9/better_solution.py
+++ day9/better_solution.py
@@ -2,7 +2,7 @@
 # will try to use recurrent functions 
 
 
-with open("input2.txt", "r") as file:
+with open("input.txt", "r") as file:
     data = file.read().split("\n")
 
 
@@ -16,11 +16,14 @@ def calculate(array):
     deltas = [y-x for x,y in pairs]
     diff = calculate(deltas)
 
-    return array[-1] + diff
+    return array[0] - diff
 
 
-array = [10, 13, 16, 21, 30, 45]
+result = 0
+for element in data:
+    element = list(map(int, element.split()))
+    result += calculate(element)
 
-print(calculate(array))
 
+print(result)
     
