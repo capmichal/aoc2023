3b49a82d8b73fe85a6cc2c13f0fce8b2ee525614
michal@dell_wsl <oolejnikmichal@gmail.com>
2024-03-19 16:10:48 +0100

    day10 part1 deque

--- day10/solution2.py
+++ day10/solution2.py
@@ -1,5 +1,6 @@
 import re
-
+from collections import deque
+# deque is used for faster append(), pop() operations that on lists --> O(1)
 
 # remember to SWITCH S to some symbol
 # remember that on top of NEW_PLACE accepting me, i also have to BE ABLE to go to NEW_PLACE
@@ -24,4 +25,14 @@ for i, line in enumerate(data):
 # got my starting point
 print(start_x, start_y)
 
+# already seen points
+seen = {(start_x, start_y)}
+queue = deque([(start_x, start_y)])
 
+"""
+this approach using deque(which can be done using list easily but with worse time-complexity)
+just adds ALL pipes, but with no particular order, len(seen) // 2 is the answer for part1, but we have
+no clue about the order of pipes
+"""
+
+# TBC
